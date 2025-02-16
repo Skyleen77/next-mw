aa77d7a471ffd64a7c8c923483f618db17cacc3d
Elliot Sutton <elsk8@outlook.fr>
2025-02-16 16:25:39 +0100

    fix(docs): add npm and github buttons

--- apps/docs/app/(home)/_components/header.tsx
+++ apps/docs/app/(home)/_components/header.tsx
@@ -1,7 +1,11 @@
+import React from 'react';
 import { useTheme } from 'next-themes';
 import { Logo } from '@workspace/ui/components/logo';
 import ThemeSwitcher from '@workspace/ui/components/ui/theme-switcher';
-import { useState } from 'react';
+import { Button } from '@workspace/ui/components/ui/button';
+import { GrNpm } from 'react-icons/gr';
+import { FaGithub } from 'react-icons/fa6';
+import Link from 'next/link';
 
 export const Header = () => {
   const { theme, setTheme } = useTheme();
@@ -11,12 +15,43 @@ export const Header = () => {
       <div className="flex items-center justify-between max-w-[1400px] w-full mx-auto px-6">
         <Logo className="lg:h-7 h-6" />
 
-        <ThemeSwitcher
-          checked={theme === 'dark'}
-          onCheckedChange={(checked) => {
-            setTheme(checked ? 'dark' : 'light');
-          }}
-        />
+        <div className="flex items-center space-x-4">
+          <div className="flex items-center space-x-2">
+            <Button
+              className="bg-[#1B1F23] hover:bg-[#1B1F23]/90 rounded-full"
+              size="icon"
+              asChild
+            >
+              <Link
+                href="https://github.com/Skyleen77/next-mw/tree/main/packages/next-mw"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <FaGithub className="!size-6 text-white npm-button" />
+              </Link>
+            </Button>
+            <Button
+              className="bg-[#CB3837] hover:bg-[#CB3837]/90 rounded-full"
+              size="icon"
+              asChild
+            >
+              <Link
+                href="https://www.npmjs.com/package/next-mw"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <GrNpm className="!size-6 text-white npm-button" />
+              </Link>
+            </Button>
+          </div>
+
+          <ThemeSwitcher
+            checked={theme === 'dark'}
+            onCheckedChange={(checked) => {
+              setTheme(checked ? 'dark' : 'light');
+            }}
+          />
+        </div>
       </div>
     </div>
   );
