88206729372aa33dea17ceb14a20665dafd728e6
Elliot Sutton <elsk8@outlook.fr>
2025-02-16 17:17:57 +0100

    feat(docs): improve sitemap

--- apps/docs/app/sitemap.ts
+++ apps/docs/app/sitemap.ts
@@ -2,8 +2,14 @@ import { source } from '@/lib/source';
 import type { MetadataRoute } from 'next';
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-  return source.getPages().map((page) => ({
-    url: `https://next-mw-docs.vercel.app${page.url}`,
-    lastModified: new Date().toISOString(),
-  }));
+  return [
+    {
+      url: 'https://next-mw-docs.vercel.app',
+      lastModified: new Date().toISOString(),
+    },
+    ...source.getPages().map((page) => ({
+      url: `https://next-mw-docs.vercel.app${page.url}`,
+      lastModified: new Date().toISOString(),
+    })),
+  ];
 }
